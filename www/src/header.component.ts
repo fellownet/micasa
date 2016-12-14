12f33767f43dc6170cbfada111b632316f5e4e6f
Bob Kersten <bob@fellownet.nl>
2016-12-14 20:55:38 +0100

    default html + css ready

--- www/src/header.component.ts
+++ www/src/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 declare var $: any;
 
@@ -9,11 +9,9 @@ declare var $: any;
 
 export class HeaderComponent {
 
-	@Input() fullname: String = "TEST";
-
 	toggleMenu(): void {
 		if ( $( 'body' ).hasClass( 'sidebar-open' ) ) {
-			$( 'body' ).removeClass( 'sidebar-open' ).removeClass( 'sidebar-collapse' );
+			$( 'body' ).removeClass( 'sidebar-open' );
 		} else {
 			$( 'body' ).addClass( 'sidebar-open' );
 		}
