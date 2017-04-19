592fb94d562cac0f7dcd7ac5554d9a18aa91c23e
Bob Kersten <bob@fellownet.nl>
2017-04-19 15:09:47 +0200

    prepare www production build

--- www/src/main.ts
+++ www/src/main.ts
@@ -2,7 +2,6 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode }         from '@angular/core';
-
 import { AppModule }              from './app.module';
 
 //enableProdMode();
